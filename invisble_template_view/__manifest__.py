e442d2552585e7485f284a63a9295afa9c4cdf21
ratan-kanak <prn@kanakinfosystems.com>
2024-04-28 17:02:03 +0530

    Updated invisble_template_view in v17.

---
+++ invisble_template_view/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Powered by Kanak Infosystems LLP.
+# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
+
+{
+    'name': 'Invisible Template View',
+    'version': '17.0.1.0',
+    'author': 'Kanak Infosystems LLP.',
+    'company': 'Kanak Infosystems LLP.',
+    'website': 'https://www.kanakinfosystems.com',
+    'depends': ['sale'],
+    'data': [
+        'report/sale_order_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
